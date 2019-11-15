6ee8192b43d979794dd3d6bc13ba42b32cbdf59d
juancadh <juancadh@gmail.com>
2019-11-15 02:23:46 +0000

    'movies_update'

--- rotten_tomatos/movies_analysis.py
+++ rotten_tomatos/movies_analysis.py
@@ -2,56 +2,224 @@ import pandas as pd
 import numpy as np
 import seaborn as sb
 from matplotlib import pyplot as plt
+from rottem_srap import rotten_tomatos
+import csv
+import datetime as dt
 
-data_movies = pd.read_csv("C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/misc_code/rotten_tomatos/movies_794406277.csv", delimiter=";")
-metadata    = pd.read_csv("C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/misc_code/rotten_tomatos/movies_metadata.csv", delimiter=";")
+# =============== USER ID OF ROTTEN TOMATOS ==============
+user_id = '794406277' #'832654610' '794406277'
+secs_to_sleep = 5
 
-metadata.rename(columns={'href': 'URL'}, inplace = True)
+myRotten        = rotten_tomatos(user_id, secs_to_sleep)
 
-data_movies = data_movies.set_index("URL")
-metadata    = metadata.set_index("URL")
+#my_movies       = myRotten.get_my_movies()
+#movies_metadata = myRotten.get_metadata_movies(",")
+all_data        = myRotten.combine_files(";")
+# ========================================================
 
-all_data = pd.merge(data_movies, metadata, left_on = "URL", right_on= "URL")
-all_data = all_data.reset_index()
 
 # Add Columns
 all_data["first_genre"]    = all_data["genre"].apply(lambda x : x.split("|")[0])
 all_data["first_director"] = all_data["director"].apply(lambda x : x.split("|")[0])
+all_data["first_actor"]    = all_data["cast"].apply(lambda x : x.split("|")[0])
 all_data["runtime"]        = all_data["runtime"].apply(lambda x : int(x.replace("minutes","").strip()))
 all_data["Rating_Score"]   = all_data["Rating"].apply(lambda x : (float(x) * 100) / 5)
+all_data['date_of_rate']   = all_data["Date Rated"].apply(lambda x : dt.datetime.strptime(x, '%d/%m/%Y') )
+all_data['year_of_rate']   = all_data['date_of_rate'].apply(lambda x : x.year )
 
-# Number of movies by year
+print_all = True
+
+# Evolution of average of rating over time
+#print(all_data.groupby('year_of_rate')['Rating'].mean())
+
+# ====================== GENERAL STATISTICS ===========
+total_movies = all_data.shape[0]
+total_runtime_h = '{:,}'.format(int(all_data["runtime"].sum()/60))
+total_runtime_d = '{:,}'.format(int((all_data["runtime"].sum()/60)/24))
+min_rate_year = all_data['year_of_rate'].min()
+max_rate_year = all_data['year_of_rate'].max()
+
+# ======================= MOST SEEN STUDIO ================
+studios_cnt = all_data.groupby("studio")["Title"].count().sort_values(ascending = False)
+
+# ======================== MOST SEEN GENRE =================
+genre_data = all_data["genre"].apply(lambda x : x.split("|"))
+all_genre = []
+for i in genre_data:
+    for j in i:
+        all_genre.append(j)
+all_genre = pd.DataFrame({"Genre" : all_genre})
+gnr_class = all_genre["Genre"].value_counts()
+top_genre = gnr_class
+
+# ======================== MOST SEEN ACTORS =================
+cast_data = all_data["cast"].apply(lambda x : x.split("|"))
+all_cast = []
+for i in cast_data:
+    for j in i:
+        all_cast.append(j)
+top_cast = pd.DataFrame({"name" : all_cast})["name"].value_counts()
+
+# ======================== MOST SEEN DIRECTOR =================
+direct_data = all_data["director"].apply(lambda x : x.split("|"))
+all_directors = []
+for i in direct_data:
+    for j in i:
+        all_directors.append(j)
+top_director = pd.DataFrame({"name" : all_directors})["name"].value_counts()
+
+# ============ Number of movies by year of movie ===============
 num_movies_by_year = all_data.groupby("Year")["Title"].count().reset_index()
 num_movies_by_year.columns = ["Year", "n_movies"]
+num_movies_by_year.sort_values("n_movies", ascending = False, inplace = True)
+num_movies_by_year.set_index("Year", inplace = True)
 
-# Summary of Genre (Number of movies, Avg Rating, Avg Audience Rating, Avg Tomato Rating)
-count_by_gender = all_data.groupby("first_genre")["Title"].count().reset_index()
-count_by_gender.columns = ["Genre", "n_movies"]
-count_by_gender = count_by_gender.sort_values("n_movies", ascending = False)
+# ============== TOP RATED FUNCTION ==================
 
+def calculate_top_rated(cat_name):
+    data = all_data[cat_name].apply(lambda x : x.split("|"))
+    data_score = pd.concat([all_data["Rating_Score"], all_data["audience_score"], all_data["tomatometer"], data], axis = 1)
+    list_gnr    = []
+    list_you_sc = []
+    list_aud_sc = []
+    list_tom_sc = []
+
+    all_vals = []
+    for i in data:
+        for j in i:
+            all_vals.append(j)
+    count_data = pd.DataFrame({"name" : all_vals})["name"].value_counts()
+
+    for i in range(len(data)):
+        your_score     = data_score.iloc[i,0]
+        audience_score = data_score.iloc[i,1]
+        tomatometer    = data_score.iloc[i,2]
+        genre_list     = data_score.iloc[i,3]
+        for j in genre_list:
+            list_gnr.append(j)
+            list_you_sc.append(your_score)
+            list_aud_sc.append(audience_score)
+            list_tom_sc.append(tomatometer)
+
+    grn_scores = pd.DataFrame({
+        "category" : list_gnr,
+        "your_score" : list_you_sc,
+        "audi_score" : list_aud_sc,
+        "toma_score" : list_tom_sc
+    })
+
+    #grn_scores.set_index("genre", inplace = True)
+    summ_gnre_score = grn_scores.groupby("category").mean().sort_values("your_score", ascending = False).round(1)
+    return summ_gnre_score
+
+
+# Top rated by gender
+summ_gnre_score = calculate_top_rated("genre")
+# Top rated by director
+summ_director_score = calculate_top_rated("director")
+# Top rated by actor
+summ_actor_score = calculate_top_rated("cast")
+
+if print_all:
+    # PRINT STUFF
+    print("\n===============================================================")
+    print("======================= GENERAL STATISTICS ====================")
+    print("===============================================================\n")
+    print(f"Total movies: {total_movies}")
+    print(f"Range of dates: {min_rate_year} - {max_rate_year}")
+    print(f"Total of hours seen: {total_runtime_h} hours")
+    print(f"Total of days seen: {total_runtime_d} days")
+    print("\n---- Most Seen -----\n")
+    print(f"    by genre   : {top_genre.index[0]} ({top_genre[0]} movies)")
+    print(f"    by actor   : {top_cast.index[0]} ({top_cast[0]} movies)")
+    print(f"    by director: {top_director.index[0]} ({top_director[0]} movies)")
+    print(f"    by Studio  : {studios_cnt.index[0]} ({studios_cnt[0]} movies)")
+    print("\n---- Top Rated -----\n")
+    print(f"    by genre   : {summ_gnre_score.index[0]} ({summ_gnre_score['your_score'][0]} Score | {summ_gnre_score['toma_score'][0]} Rotten Score)")
+    #print(f"    by actor   : {summ_actor_score.index[0]} ({summ_actor_score['your_score'][0]} Score | {summ_actor_score['toma_score'][0]} Rotten Score)")
+    #print(f"    by director: {summ_director_score.index[0]} ({summ_director_score['your_score'][0]} Score | {summ_director_score['toma_score'][0]} Rotten Score)")
+
+    print("\n---- Worst Rated -----\n")
+    print(f"    by genre   : {summ_gnre_score.index[-1]} ({summ_gnre_score['your_score'][-1]} Score | {summ_gnre_score['toma_score'][-1]} Rotten Score)")
+    #print(f"    by actor   : {summ_actor_score.index[-1]} ({summ_actor_score['your_score'][-1]} Score | {summ_actor_score['toma_score'][-1]} Rotten Score)")
+    #print(f"    by director: {summ_director_score.index[-1]} ({summ_director_score['your_score'][-1]} Score | {summ_director_score['toma_score'][-1]} Rotten Score)")
+
+
+    print("\n===============================================================")
+    print("========================= MOST SEEN ===========================")
+    print("===============================================================")
+    print("\n================= MOST SEEN BY GENRE =================\n")
+    print(top_genre.head(5))
+    print("\n================= MOST SEEN BY CAST =================\n")
+    print(top_cast.head(5))
+    print("\n================= MOST SEEN BY DIRECTOR =================\n")
+    print(top_director.head(5))
+    print("\n================= MOST SEEN BY YEAR =================\n")
+    print(num_movies_by_year.head(5))
+    print("\n================= MOST SEEN BY STUDIO =================\n")
+    print(studios_cnt.head(5))
+
+    print("\n===============================================================")
+    print("========================= TOP RATED ===========================")
+    print("===============================================================")
+    print("\n================= TOP RATED BY GENRE =================\n")
+    print(summ_gnre_score.head(5))
+    #print("\n================= TOP RATED BY CAST =================\n")
+    #print(summ_actor_score.head(5))
+    #print("\n================= TOP RATED BY DIRECTOR =================\n")
+    #print(summ_director_score.head(5))
+
+
+
+# Chart of Evolution in rating fequency over Time
+rate_series = all_data['year_of_rate'].value_counts(sort = False)
+sb.set()
+plt.plot(rate_series)
+plt.ylabel("Number of movies")
+plt.title("Evolution of rating")
+plt.show()
+
+""" 
+# ============= Summary of Genre ==========================
+# (Number of movies, Avg Rating, Avg Audience Rating, Avg Tomato Rating)
 rating_by_gender = all_data.groupby("first_genre")[["Rating_Score","audience_score", "tomatometer"]].mean().reset_index()
 rating_by_gender.columns = ["Genre", "Rating","Audience_Rating","Tomato_Rating"]
 rating_by_gender = rating_by_gender.sort_values("Rating", ascending = False)
 
-genre_anly = pd.merge(left = rating_by_gender, right = count_by_gender, left_on = "Genre", right_on = "Genre")
-print(genre_anly)
+genre_anly = pd.merge(left = rating_by_gender, right = gnr_class, left_on = "Genre", right_on = "Genre")
 
-f, axes = plt.subplots(2, 2, figsize=(7, 7), sharex=True)
+# ======================== PLOT FINDINGS =======================
+f, axes = plt.subplots(2, 2, figsize=(14, 6))
+f.subplots_adjust(hspace = 0.3)
 sb.despine(left=True)
 
-# Joinplot of Rating vs Tomato Rating
-#sb.jointplot(x = all_data["Rating_Score"], y = all_data["tomatometer"], kind="hex", color="#4CB391", ax = axes[0])
-sb.barplot(num_movies_by_year["Year"], num_movies_by_year["n_movies"], ax = axes[0,0])
+# Gener by count
+genre_anly.sort_values('n_movies', ascending = True, inplace = True)
+genre_anly.set_index("Genre", inplace = True)
+genre_anly.rename(index = {
+    'Comedy' : 'Comedy',
+    'Drama' : 'Drama',
+    'Action & Adventure' : 'Action',
+    'Art House & International' : 'Intern.',
+    'Animation' : 'Animation',
+    'Horror' : 'Horro',
+    'Mystery & Suspense' : 'Suspense',
+    'Classics' : 'Classic',
+    'Documentary' : 'Documentary',
+    'Kids & Family' : 'Kids',
+    'Musical & Performing Arts' : 'Musical',
+    'Romance' : 'Romance'
+}, inplace = True)
+axes[0,0].barh(list(genre_anly.index), genre_anly["n_movies"], align='center', color="lightblue")
+axes[0,0].set_title("Count by Genre")
 
 # Number of movies by year
-sb.barplot(num_movies_by_year["Year"], num_movies_by_year["n_movies"], ax = axes[1,0])
-plt.title("Number of movies by year")
+sb.barplot(num_movies_by_year["Year"][20:], num_movies_by_year["n_movies"][20:], ax = axes[1,1])
+axes[1,1].set_title("Count by Year of the Movie")
 plt.ylabel("Number of movies")
-plt.xlabel("Year")
 plt.xticks(rotation = 90)
 
-plt.setp(axes, yticks=[])
-plt.tight_layout()
-plt.show()
+#plt.show()
 
 #print(all_data[["Title", "Year", "first_genre", "first_director", "Rating", "Rating_Score", "audience_score", "tomatometer","runtime"]])
+ """
--- rotten_tomatos/rottem_srap.py
+++ rotten_tomatos/rottem_srap.py
@@ -1,87 +1,258 @@
-from selenium import webdriver
 import os
 import sys
 import datetime
 import time
 import csv
+import pandas as pd
+import numpy as np
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+capa = DesiredCapabilities.CHROME
+capa["pageLoadStrategy"] = "none"
 
 # =============== USER ID OF ROTTEN TOMATOS ==============
-user_id = '794406277' #'832654610'
-secs_to_sleep = 180
+#user_id = '794406277' #'832654610'
+#secs_to_sleep = 180
+
+#myRotten        = rotten_tomatos(user_id, secs_to_sleep)
+#my_movies       = myRotten.get_my_movies()
+#movies_metadata = get_metadata_movies()
 # ========================================================
 
-browser = webdriver.Chrome(executable_path="C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/misc_code/rotten_tomatos/chromedriver")
-browser.get(f'https://www.rottentomatoes.com/user/id/{user_id}/ratings')
-time.sleep(secs_to_sleep)
+class rotten_tomatos():
 
-all_items = browser.find_elements_by_xpath("//*[@id='col_right_index']/section/div/ul/li")
+    def __init__(self, user_id, secs_to_sleep):
+        self.user_id = user_id
+        self.secs_to_sleep = secs_to_sleep
 
-# Create CSV File
-f = csv.writer(open("C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/misc_code/rotten_tomatos/movies_" + user_id + ".csv", "w"), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-f.writerow(['Title','Year', 'Rating', 'Date Rated', 'URL', 'Poster URL'])
+    # ===================================================================
+    # GET MOVIES OF AN SPECIFIC USER ID
+    # ====================================================================
+    def get_my_movies(self):
 
-movies = {}
-c = 0
-for i in all_items:
-    print(f". . . Getting movie {c+1} of {len(all_items)}")
+        browser = webdriver.Chrome(executable_path="C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/chromedriver")
+        browser.get(f'https://www.rottentomatoes.com/user/id/{self.user_id}/ratings')
+        print("(!) REALIZA EL SCROLL EN LA PAGINA HASTA LLEGAR A LA ULTIMA PELICULA.")
+        time.sleep(self.secs_to_sleep)
 
-    try:
-        # Get the title
-        title = i.find_element_by_css_selector("a.ratings__movie-title").text.strip().capitalize().replace(",","")
-        # Get the Hyperlink
-        href  = i.find_element_by_css_selector("a.ratings__movie-title").get_attribute("href")
-        # Get the Year
-        year  = i.find_element_by_css_selector("span.small.subtle").text.strip().capitalize().replace("(","").replace(")","")
-        # Get the Rating Stars
-        rating_obj = i.find_element_by_css_selector("span.star-display").find_elements_by_css_selector("span")
-        rating = 0
-        for start in rating_obj:
-            class_name = start.get_attribute("class").strip()
-            if class_name == "star-display__filled":
-                rating = rating + 1
-            elif class_name == "star-display__half":
-                rating = rating + 0.5
-            else:
-                rating = rating + 0
+        all_items = browser.find_elements_by_xpath("//*[@id='col_right_index']/section/div/ul/li")
 
-        # Get the URL of the picture 
-        url_poster = i.find_element_by_css_selector("img.media-object.rating__posterArt").get_attribute("src")
+        # Create CSV File
+        f = open("C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/movies_" + self.user_id + ".csv", 'w', newline='')
+        theFile = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        theFile.writerow(['Title','Year', 'Rating', 'Date Rated', 'URL', 'Poster URL'])
 
-        # Get the time when it was seen
-        date_of_rated = i.find_element_by_css_selector("a.ratings__age").text.strip().capitalize()
-        value, unit = date_of_rated.split()[0:2]
+        movies = {}
+        c = 0
+        for i in all_items:
+            print(f". . . Getting movie {c+1} of {len(all_items)}")
 
-        if (unit == "minute") or (unit == "hour") or (unit == "day"):
-            unit = unit + "s"
+            try:
+                # Get the title
+                title = i.find_element_by_css_selector("a.ratings__movie-title").text.strip().capitalize().replace(",","")
+                # Get the Hyperlink
+                href  = i.find_element_by_css_selector("a.ratings__movie-title").get_attribute("href")
+                # Get the Year
+                year  = i.find_element_by_css_selector("span.small.subtle").text.strip().capitalize().replace("(","").replace(")","")
+                # Get the Rating Stars
+                rating_obj = i.find_element_by_css_selector("span.star-display").find_elements_by_css_selector("span")
+                rating = 0
+                for start in rating_obj:
+                    class_name = start.get_attribute("class").strip()
+                    if class_name == "star-display__filled":
+                        rating = rating + 1
+                    elif class_name == "star-display__half":
+                        rating = rating + 0.5
+                    else:
+                        rating = rating + 0
 
-        if (unit == "month") or (unit == "months"):
-            unit = "days"
-            value = int(value) * 30
+                # Get the URL of the picture 
+                url_poster = i.find_element_by_css_selector("img.media-object.rating__posterArt").get_attribute("src")
 
-        if (unit == "year") or (unit == "years"):
-            unit = "days"
-            value = int(value) * 365
+                # Get the time when it was seen
+                date_of_rated = i.find_element_by_css_selector("a.ratings__age").text.strip().capitalize()
+                value, unit = date_of_rated.split()[0:2]
 
-        dt = datetime.timedelta(**{unit: float(value)})
-        date_rated_2 = datetime.datetime.now() - dt
-        date_rated_2 = date_rated_2.strftime("%d-%m-%Y")
+                if (unit == "minute") or (unit == "hour") or (unit == "day"):
+                    unit = unit + "s"
 
-        movies[c] = {"title"  : title,
-                    "year"   : year, 
-                    "rating" : rating, 
-                    "date_of_rated" : date_rated_2, 
-                    "href"   : href,
-                    "url_poster" : url_poster}
+                if (unit == "month") or (unit == "months"):
+                    unit = "days"
+                    value = int(value) * 30
 
-        f.writerow([title, year, rating, date_rated_2, href, url_poster])
-    except:
-        print(f"Error charging this movie: {title}")
-        pass
+                if (unit == "year") or (unit == "years"):
+                    unit = "days"
+                    value = int(value) * 365
 
-    c += 1
+                dt = datetime.timedelta(**{unit: float(value)})
+                date_rated_2 = datetime.datetime.now() - dt
+                date_rated_2 = date_rated_2.strftime("%d-%m-%Y")
 
-browser.close()
+                movies[c] = {"title"  : title,
+                            "year"   : year, 
+                            "rating" : rating, 
+                            "date_of_rated" : date_rated_2, 
+                            "href"   : href,
+                            "url_poster" : url_poster}
 
-print(f"========= TOTAL PELICULAS IMPORTADAS: {c}")
-#print(movies)
+                theFile.writerow([title, year, rating, date_rated_2, href, url_poster])
+            except:
+                print(f"Error charging this movie: {title}")
+                pass
+
+            c += 1
+
+        f.close()
+        browser.close()
+
+        print(f"========= TOTAL PELICULAS IMPORTADAS: {c} ===========")
+        return movies
+
+    # ============================================================================
+    # GET THE METADATA OF MY MOVIES
+    # ============================================================================
+    def get_metadata_movies(self, CSVdelimeter = ";"):
+
+        data_movies = pd.read_csv(f"C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/movies_{self.user_id}.csv", delimiter=CSVdelimeter)
+        url_movies = data_movies['URL']
+
+        # Create CSV File
+        f = open(f"C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/movies_metadata_{self.user_id}.csv", 'w', newline='')
+        myFile = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        myFile.writerow(['href','tomatometer', 'audience_score', 'genre', 'director', 'runtime', 'studio', 'cast', 'date'])
+
+        c = 0
+        result = {}
+
+        for url in url_movies:
+            print(f"\nRecopilando info de {url} | {c+1} de {len(url_movies)}")
+
+            browser = webdriver.Chrome(executable_path="C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/chromedriver", desired_capabilities= capa)
+            wait = WebDriverWait(browser, 20)
+            browser.get(url)
+
+            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.mop-ratings-wrap__half')))
+            browser.execute_script("window.stop();")
+
+            try:
+                # Tomato Meter
+                tomatometer = browser.find_element_by_css_selector("div.mop-ratings-wrap__half").find_element_by_css_selector("span.mop-ratings-wrap__percentage").text.strip().replace("%","")
+            except:
+                tomatometer = ''
+
+            try:
+                # Audience Score
+                audience_score = browser.find_element_by_css_selector("div.mop-ratings-wrap__half.audience-score").find_element_by_css_selector("span.mop-ratings-wrap__percentage").text.strip().replace("%","")
+            except:
+                audience_score = ''
+
+            try:
+                # List of meta data
+                list_meta_data = browser.find_element_by_css_selector("ul.content-meta.info").find_elements_by_css_selector("li.meta-row.clearfix")
+            except:
+                list_meta_data = []
+            
+            for i in list_meta_data:
+                meta_label = i.find_element_by_css_selector("div.meta-label.subtle").text.strip()
+
+                try:
+                    # Genre
+                    if meta_label == "Genre:":
+                        genre_obj = i.find_element_by_css_selector("div.meta-value").find_elements_by_css_selector("a")
+                        genre = []
+                        for gnr in genre_obj:
+                            genre.append(gnr.text.strip().replace(",",""))
+                        
+                        genre_str = "|".join(genre)
+                except:
+                    genre = []
+                    genre_str = ""
+
+                try:
+                    # Director
+                    if meta_label == "Directed By:":
+                        dir_obj = i.find_element_by_css_selector("div.meta-value").find_elements_by_css_selector("a")
+                        director = []
+                        for d in dir_obj:
+                            director.append(d.text.strip().replace(",",""))
+                        director_str = "|".join(director)
+                except:
+                    director = []
+                    director_str = ""
+
+                try:
+                    # Runtime
+                    if meta_label == "Runtime:":
+                        runtime = i.find_element_by_css_selector("div.meta-value").find_element_by_css_selector("time").text.strip()
+                except:
+                    runtime = ''
+
+                try:
+                    # Studio 
+                    if meta_label == "Studio:":
+                        studio = i.find_element_by_css_selector("div.meta-value").text.strip()
+                except:
+                    studio = ''
+
+                try: 
+                    # In Theaters
+                    if meta_label == "In Theaters:":
+                        date_tht = i.find_element_by_css_selector("div.meta-value").find_element_by_css_selector("time").get_attribute("datetime")
+                except:
+                    date_tht = ''
+            
+            try:
+                # Cast
+                cast_obj = browser.find_elements_by_css_selector("div.cast-item.media.inlineBlock")
+                cast = []
+                for i in cast_obj:
+                    actor_name = i.find_element_by_css_selector("div.media-body").find_element_by_css_selector("span").text.strip().replace(",","")
+                    if len(actor_name) > 0:
+                        cast.append(actor_name)
+                    cast_str = "|".join(cast)
+            except: 
+                cast = []
+                cast_str = ""
+
+            result[c] = {
+                "href" : url,
+                "tomatometer" : tomatometer,
+                "audience_score" : audience_score,
+                "genre" : genre,
+                "director" : director,
+                "runtime" : runtime,
+                "studio" : studio,
+                "cast" : cast,
+                "date_tht" : date_tht
+            }
+
+            myFile.writerow([url, tomatometer, audience_score, genre_str, director_str, runtime, studio, cast_str, date_tht])
+
+            c += 1
+
+            browser.close()
+
+        f.close()
+        return result
+
+    def combine_files(self, CSVdelimeter = ";"):
+        data_movies = pd.read_csv(f"C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/movies_{self.user_id}.csv", delimiter=CSVdelimeter)
+        metadata    = pd.read_csv(f"C:/Users/Juan Camilo Díaz/Dropbox/Project_VSC/python_projects/rotten_tomatos/movies_metadata_{self.user_id}.csv", delimiter=CSVdelimeter)
+
+        metadata.rename(columns={'href': 'URL'}, inplace = True)
+
+        data_movies = data_movies.set_index("URL")
+        metadata    = metadata.set_index("URL")
+
+        all_data = pd.merge(data_movies, metadata, left_on = "URL", right_on= "URL")
+        all_data = all_data.reset_index()
+
+        return all_data
+
+
+
+
 
